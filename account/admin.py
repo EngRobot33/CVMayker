01a4081a84453ac70914ea6fced5d92e51343385
EngRobot33 <hmdkhsrvee@gmail.com>
2022-12-02 17:41:58 +0330

    Add account models and admins

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class HumanResourcesAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name',)
+
+
+admin.site.register(HumanResources, HumanResourcesAdmin)
+
+
+class JobSeekerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'get_skill', 'project')
+
+    def get_skill(self, obj):
+        return [skill.title for skill in obj.skill.all()]
+
+
+admin.site.register(JobSeeker, JobSeekerAdmin)
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Skill, SkillAdmin)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Project, ProjectAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,69 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from utils.models import BaseModel
+
+
+class HumanResources(AbstractUser):
+    username_validator = ASCIIUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=128,
+        unique=True,
+        help_text=_('Required. 128 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=128)
+    last_name = models.CharField(_('last name'), max_length=128)
+
+    def __str__(self):
+        return f'{self.username}'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        db_table = 'user'
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=64, verbose_name=_('name'), unique=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = _('skill')
+        verbose_name_plural = _('skills')
+        db_table = 'skill'
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=64, verbose_name=_('name'), unique=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = _('project')
+        verbose_name_plural = _('projects')
+        db_table = 'project'
+
+
+class JobSeeker(BaseModel):
+    first_name = models.CharField(_('first name'), max_length=128)
+    last_name = models.CharField(_('last name'), max_length=128)
+    email = models.EmailField(_('email address'), unique=True)
+    phone_regex = RegexValidator(regex=r'^(\+98|0)?9\d{9}$',
+                                 message="Phone number must be entered in the format: '+989123456789/09123456789'.")
+    phone_number = models.CharField(verbose_name=_('phone number'), validators=[phone_regex], max_length=13, blank=True)
+    skills = models.ManyToManyField(Skill, verbose_name=_('skill'))
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name=_('project'))
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
