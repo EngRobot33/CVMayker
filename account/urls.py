f83b92ee0bc3681559f2639455207e179e222cf3
EngRobot33 <hmdkhsrvee@gmail.com>
2022-12-02 20:34:09 +0330

    Add index view and template

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import *
+
+app_name = 'account'
 
 urlpatterns = [
-
+    path('', index, name='index'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('account.urls')),
+    path('', include('account.urls')),
 ]
